ad7c83c56551ae1862dc6bd50981349c45dd383b
jnemcic <josipa.nemcic@gmail.com>
2023-02-02 20:17:06 +0100

    First commit. Programatic UI control.

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var selectedHeightValue: UILabel!
+    @IBOutlet weak var selectedWeightValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func heightSelection(_ sender: UISlider) {
+        selectedHeightValue.text =  "\(String(format: "%.2f", sender.value))m"
+    }
+    
+    @IBAction func weightSelection(_ sender: UISlider) {
+        selectedWeightValue.text = "\(String(format: "%.1f", sender.value))Kg" //String(round(sender.value * 10) / 10))
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / pow(height, 2)
+        
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        
+        self.present(secondVC, animated: true, completion: nil)
+    }
 }
 
